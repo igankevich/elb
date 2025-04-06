369d5cf0489ea9d687c0e5f51316ab260e43616c
Ivan Gankevich <ivan@igankevich.com>
2025-04-06 11:32:33 +0200

    Fix base32 tests.

--- dl/src/base32.rs
+++ dl/src/base32.rs
@@ -1,18 +1,22 @@
 #![allow(missing_docs)]
 
+pub const MAX_INPUT_LEN: usize = usize::MAX / 8 * 5 + 4;
+
 pub const fn encoded_len(input_len: usize) -> usize {
-    debug_assert!(input_len / 5 <= usize::MAX / 8);
+    if input_len > MAX_INPUT_LEN {
+        panic!("The input is too large");
+    }
     input_len / 5 * 8
         + match input_len % 5 {
             0 => 0,
-            1 => 1,
+            1 => 2,
             2 => 4,
             3 => 5,
             _ => 7,
         }
 }
 
-pub const fn decoded_len(input_len: usize) -> usize {
+pub const fn max_decoded_len(input_len: usize) -> usize {
     input_len / 8 * 5
         + match input_len % 8 {
             0 => 0,
@@ -26,7 +30,9 @@ pub const fn decoded_len(input_len: usize) -> usize {
 pub fn encode_into(input: &[u8], output: &mut [u8]) {
     let input_len = input.len();
     let output_len = output.len();
-    debug_assert!(encoded_len(input_len) <= output.len());
+    if encoded_len(input_len) > output.len() {
+        panic!("Output slice is too small");
+    }
     let aligned_input_len = input_len / 5 * 5;
     let aligned_output_len = output_len / 8 * 8;
     let input_iter = (0..aligned_input_len).step_by(5);
@@ -54,12 +60,12 @@ pub fn encode_into(input: &[u8], output: &mut [u8]) {
     let j = aligned_input_len;
     let a = input[j];
     output[i] = CHARS[(a & 0b11111) as usize]; // 5 bits
+    let b = input.get(j + 1).copied().unwrap_or(0);
+    output[i + 1] = CHARS[((a >> 5) | ((b & 0b11) << 3)) as usize]; // 3 + 2 bits
     if remaining == 1 {
         return;
     }
-    let b = input[j + 1];
     let c = input.get(j + 2).copied().unwrap_or(0);
-    output[i + 1] = CHARS[((a >> 5) | ((b & 0b11) << 3)) as usize]; // 3 + 2 bits
     output[i + 2] = CHARS[((b >> 2) & 0b11111) as usize]; // 5 bits
     output[i + 3] = CHARS[((b >> 7) | ((c & 0b1111) << 1)) as usize]; // 1 + 4 bits
     if remaining == 2 {
@@ -75,10 +81,13 @@ pub fn encode_into(input: &[u8], output: &mut [u8]) {
     output[i + 6] = CHARS[((d >> 6) | ((e & 0b111) << 2)) as usize]; // 2 + 3 bits
 }
 
-pub fn decode_into(input: &[u8], output: &mut [u8]) {
+#[must_use]
+pub fn decode_into(input: &[u8], output: &mut [u8]) -> usize {
     let input_len = input.len();
     let output_len = output.len();
-    debug_assert!(decoded_len(input_len) <= output.len());
+    if max_decoded_len(input_len) > output.len() {
+        panic!("Output slice is too small");
+    }
     let aligned_input_len = input_len / 8 * 8;
     let aligned_output_len = output_len / 5 * 5;
     let input_iter = (0..aligned_input_len).step_by(8);
@@ -93,14 +102,14 @@ pub fn decode_into(input: &[u8], output: &mut [u8]) {
         let g = char_index(input[j + 6]);
         let h = char_index(input[j + 7]);
         output[i] = a | ((b & 0b111) << 5); // 5 + 3 bits
-        output[i + 1] = (b >> 3) | (c << 2) | ((d & 0b1) << 6); // 2 + 5 + 1 bits
+        output[i + 1] = (b >> 3) | (c << 2) | ((d & 0b1) << 7); // 2 + 5 + 1 bits
         output[i + 2] = (d >> 1) | ((e & 0b1111) << 4); // 4 + 4 bits
         output[i + 3] = (e >> 4) | (f << 1) | ((g & 0b11) << 6); // 1 + 5 + 2 bits
         output[i + 4] = (g >> 2) | (h << 3); // 3 + 5 bits
     }
     let remaining = input_len - aligned_input_len;
     if remaining == 0 {
-        return;
+        return output_len;
     }
     let i = aligned_output_len;
     let j = aligned_input_len;
@@ -108,22 +117,27 @@ pub fn decode_into(input: &[u8], output: &mut [u8]) {
     let b = input.get(j + 1).copied().map(char_index).unwrap_or(0);
     output[i] = a | ((b & 0b111) << 5); // 5 + 3 bits
     if remaining == 1 {
-        return;
+        return i + 1;
     }
     let c = input.get(j + 2).copied().map(char_index).unwrap_or(0);
     let d = input.get(j + 3).copied().map(char_index).unwrap_or(0);
-    output[i + 1] = (b >> 3) | (c << 2) | ((d & 0b1) << 6); // 2 + 5 + 1 bits
+    output[i + 1] = (b >> 3) | (c << 2) | ((d & 0b1) << 7); // 2 + 5 + 1 bits
     if remaining == 2 || remaining == 3 {
-        return;
+        return if output[i + 1] == 0 { i + 1 } else { i + 2 };
     }
     let e = input.get(j + 4).copied().map(char_index).unwrap_or(0);
     output[i + 2] = (d >> 1) | ((e & 0b1111) << 4); // 4 + 4 bits
     if remaining == 4 {
-        return;
+        return if output[i + 2] == 0 { i + 2 } else { i + 3 };
     }
     let f = input.get(j + 5).copied().map(char_index).unwrap_or(0);
     let g = input.get(j + 6).copied().map(char_index).unwrap_or(0);
     output[i + 3] = (e >> 4) | (f << 1) | ((g & 0b11) << 6); // 1 + 5 + 2 bits
+    if output[i + 3] == 0 {
+        i + 3
+    } else {
+        i + 4
+    }
 }
 
 const fn char_index(ch: u8) -> u8 {
@@ -135,16 +149,16 @@ const fn char_index(ch: u8) -> u8 {
 // Crockford's base32.
 const CHARS: [u8; 32] = *b"0123456789abcdefghjkmnpqrstvwxyz";
 
-const INDICES_1: [u8; 26] = [
-    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, NA,
+const INDICES: [[u8; 26]; 2] = [
+    [
+        NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        NA,
+    ],
+    [
+        10, 11, 12, 13, 14, 15, 16, 17, NA, 18, 19, NA, 20, 21, NA, 22, 23, 24, 25, 26, NA, 27, 28,
+        29, 30, 31,
+    ],
 ];
-
-const INDICES_2: [u8; 26] = [
-    10, 11, 12, 13, 14, 15, 16, 17, NA, 18, 19, NA, 20, 21, NA, 22, 23, 24, 25, 26, NA, 27, 28, 29,
-    30, 31,
-];
-
-const INDICES: [[u8; 26]; 2] = [INDICES_1, INDICES_2];
 
 const NA: u8 = b'_';
 
@@ -162,18 +176,88 @@ mod tests {
     }
 
     #[test]
+    fn test_encoded_len() {
+        arbtest(|u| {
+            let input_len = u.int_in_range(0..=usize::MAX / 8)?;
+            let enc_len = encoded_len(input_len);
+            let max_dec_len = max_decoded_len(enc_len);
+            assert!(
+                input_len == max_dec_len || input_len + 1 == max_dec_len,
+                "input len = {input_len}, encoded len = {enc_len}, max decoded len = {max_dec_len}"
+            );
+            Ok(())
+        });
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_encoded_len_panic() {
+        let _enc_len = encoded_len(MAX_INPUT_LEN + 1);
+    }
+
+    #[test]
     fn test_encode() {
         let input = *b"hello";
         let mut output = [b'_'; encoded_len(5)];
         encode_into(&input, &mut output[..]);
         eprintln!("{}", std::str::from_utf8(&output[..]).unwrap());
         let mut decoded = [b'_'; 5];
-        decode_into(&output, &mut decoded);
+        let len = decode_into(&output, &mut decoded);
+        assert_eq!(5, len);
+        assert_eq!(input, decoded);
         eprintln!("{}", std::str::from_utf8(&decoded[..]).unwrap());
     }
 
     #[test]
-    fn test_arbitrary() {
+    fn test_len_divisible_by_5() {
+        arbtest(|u| {
+            let input_len: usize = u.arbitrary_len::<u8>()? * 5;
+            let mut input = Vec::with_capacity(input_len);
+            for _ in 0..input_len {
+                input.push(u.arbitrary()?);
+            }
+            let mut encoded = vec![b'_'; encoded_len(input.len())];
+            encode_into(&input, &mut encoded);
+            assert!(
+                !encoded.contains(&b'_'),
+                "input = {:?}, encoded = {:?}",
+                input,
+                std::str::from_utf8(&encoded)
+            );
+            let mut decoded = vec![0_u8; max_decoded_len(encoded.len())];
+            let len = decode_into(&encoded, &mut decoded);
+            assert_eq!(input_len, len);
+            assert_eq!(input, decoded);
+            Ok(())
+        });
+    }
+
+    #[test]
+    fn test_len_non_divisible_by_5() {
+        arbtest(|u| {
+            let input_len: usize = u.int_in_range(0..=4)?;
+            let mut input = Vec::with_capacity(input_len);
+            for _ in 0..input_len {
+                input.push(u.arbitrary()?);
+            }
+            let mut encoded = vec![b'_'; encoded_len(input.len())];
+            encode_into(&input, &mut encoded);
+            assert!(
+                !encoded.contains(&b'_'),
+                "input = {:?}, encoded = {:?}",
+                input,
+                std::str::from_utf8(&encoded)
+            );
+            let mut decoded = vec![0_u8; max_decoded_len(encoded.len())];
+            let len = decode_into(&encoded, &mut decoded);
+            let decoded = &decoded[..len];
+            assert_eq!(input, decoded);
+            Ok(())
+        });
+    }
+
+    #[test]
+    fn test_any_len() {
         arbtest(|u| {
             let input: Vec<u8> = u.arbitrary()?;
             let mut encoded = vec![b'_'; encoded_len(input.len())];
@@ -184,8 +268,10 @@ mod tests {
                 input,
                 std::str::from_utf8(&encoded)
             );
-            let mut decoded = vec![b'_'; decoded_len(encoded.len())];
-            decode_into(&encoded, &mut decoded);
+            let mut decoded = vec![0_u8; max_decoded_len(encoded.len())];
+            let len = decode_into(&encoded, &mut decoded);
+            let decoded = &decoded[..len];
+            assert_eq!(input, decoded);
             Ok(())
         });
     }
